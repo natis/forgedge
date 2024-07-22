e7cc85398fc32cd3e2d625479a38dd234655e318
natis <jjoven@craftylab.com>
2024-07-22 09:58:59 -0400

    Delete forgedge directory

--- forgedge/src/components/breadcrumbs.svelte
+++
@@ -1,7 +0,0 @@
-<nav class="breadcrumbs">
-	<ul>
-		<li><a href="/">Top Level Page</a></li>
-		<li><a href="/">Section Page</a></li>
-		<li>Current Page</li>
-	</ul>
-</nav><!-- /breadcrumbs -->
--- forgedge/src/components/topic-card.svelte
+++
@@ -1,7 +0,0 @@
-<div class="topic-card">
-	<div class="topic-card-content">
-		<h2 class="topic-card-title">Topic Card Headline</h2>
-		<p><a href="/">Policy Hub</a> provides two forums that draw on the expertise of <a href="/">Atlanta Fed economists and researcher</a> to address issues of broad policy interest: <a href="/">Macroblog</a> and <a href="/">Policy Hub Papers</a>.</p>
-	</div><!--/topic-card-->
-	<img src="http://fpoimg.com/420x210?text=Topic Card" width="420" height="210" alt="FPO" />
-</div><!-- /topic-card -->
